35deec5d7c792ea3e0df1208e22646bff88ab5bb
AMIT <amit@amits-macbook-pro.local>
2018-07-24 14:56:54 +0200

    Initial Commit

---
+++ pager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pager
+//
+//  Created by AMIT on 7/24/18.
+//  Copyright © 2018 com.hgdhghdgfd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
